0f8290462d2c7e22936f07c21b83cccd8818521e
jollywatt <jo.alex.w@gmail.com>
2025-02-26 12:59:04 +0000

    support CGA

--- BGVClient/ext/GeometricAlgebraExt.jl
+++ BGVClient/ext/GeometricAlgebraExt.jl
@@ -6,7 +6,7 @@ import BGVClient: encode
 import BGVClient.Pickle: List
 
 using GeometricAlgebra: replace_signature
-using GeometricAlgebra.Algebras: Projective
+using GeometricAlgebra.Algebras: Projective, CGA
 
 List(a::GeometricAlgebra.SingletonVector) = List(collect(a))
 
@@ -30,8 +30,8 @@ const PointBasedEuclidean = Projective{4,I,false} where I
 
 originvector(sig::Type{<:Projective{Sig,I}}) where {Sig,I} = basis(sig, 1, I)
 
-projcomponent(a::Multivector{<:Projective{Sig,I},1}) where {Sig,I} = a.comps[I]
-function nonprojcomponents(a::Multivector{<:Projective{Sig,I},1}) where {Sig,I}
+projcomp(a::Multivector{<:Projective{Sig,I},1}) where {Sig,I} = a.comps[I]
+function nonprojcomps(a::Multivector{<:Projective{Sig,I},1}) where {Sig,I}
 	if I == 1
 		a.comps[2:end]
 	elseif I == dimension(Sig)
@@ -41,7 +41,7 @@ function nonprojcomponents(a::Multivector{<:Projective{Sig,I},1}) where {Sig,I}
 	end
 end
 
-projpoint(a::Grade{1}) = nonprojcomponents(a)/projcomponent(a)
+projpoint(a::Grade{1}) = nonprojcomps(a)/projcomp(a)
 
 
 
@@ -61,7 +61,7 @@ function encode(plane::Multivector{<:PointBasedEuclidean,3})
 	e0 = originvector(signature(plane))
 	reciprocal_point = rdual(plane)::Grade{1}
 	origin = projpoint((e0∧reciprocal_point)∨plane)
-	normal = nonprojcomponents((rdual(plane)∧e0)⨽e0)
+	normal = nonprojcomps((rdual(plane)∧e0)⨽e0)
 
 	Dict("Infinite Grid" => [(origin, origin + normal)])
 end
@@ -73,9 +73,58 @@ function encodable(a::Multivector{Projective{Sig,I,PlaneBased}}) where {Sig,I,Pl
 	replace_signature(pointbased, Val(euclideansig))
 end
 
+encodable(a::Grade{0}) = scalar(a)
+
 
 encode(a::BasisBlade) = encode(Multivector(a))
 encode(a::Multivector) = encode(encodable(a))
+
+
+#= conformal =#
+
+using GeometricAlgebra.Algebras: up, dn, cgabasis
+
+basecomps(a::Multivector{CGA{n},1}) where n = a.comps[1:n]
+
+function encode(X::Multivector{CGA{3},1})
+	e0, eoo = cgabasis(signature(X))
+
+	norm = -(eoo⊙X)
+	if abs(norm) < eps()
+		# has no e0 component; is a plane
+		normal = basecomps(X)
+		ℓ = -2\(X⊙e0)
+		origin = normal*ℓ
+		Dict("Infinite Grid" => [(origin, origin + normal)])
+
+	else
+		X /= norm
+		x = basecomps(X)
+		ρ² = X⊙X
+		if abs(ρ²) < 1e-3
+			Dict("Simple Point" => [x])
+		else
+			r = sign(ρ²)sqrt(abs(ρ²))
+			Dict("Simple Sphere" => [(x, r)])
+		end
+	end
+
+end
+
+function normalize(X::Multivector{<:CGA,1})
+	e0, eoo = cgabasis(signature(X))
+	X/-(eoo⊙X)
+end
+
+
+function encode(X::Multivector{CGA{3},4})
+	encode(hodgedual(X))
+end
+
+
+
+#= spherical =#
+
 
 
 
