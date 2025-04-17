4d33cc500654d01ad23c045b851156cb974e8846
jollywatt <jo.alex.w@gmail.com>
2025-04-17 11:58:09 +0100

    tidy animation examples

--- examples.jl
+++ examples.jl
@@ -22,28 +22,25 @@ end
 function anim_toridal_rotor()
 	o, oo = Conformal.origin(CGA{3}), Conformal.infinity(CGA{3})
 
+	# random points near origin
 	x = up.(CGA, randn(Multivector{3,1}, 50))
 
-	s = wedge(rand(x, 3)...)
-	S = wedge(rand(x, 3)...)
+	# draw circles through random triplets of points
+	circles = [wedge(rand(x, 3)...) for _ in 1:3]
 
+	# create a normalized circle to define the rotor
 	p = up.(CGA, randn(Multivector{3,1}, 3))
-	c = wedge(p...)
-
+	c = wedge(randn(x, 3)...)
 	c /= sqrt(c⊙c)
 
-	R(t) = exp(t*hodgedual(c))
 	animate(range(0, π, length=300)) do t
-		Rt = R(t)
-		y = sandwich_prod.(Rt, x)
+		Rt = exp(t*hodgedual(c))
 		[
-			Styled(c, color=(1,0.5,0,1))
-			y
-			sandwich_prod(Rt, s)
-			sandwich_prod(Rt, S)
+			Styled(c, color=(1,0.5,0,1)) # RGBA for orange circle
+			sandwich_prod.(Rt, x) # rotate points
+			sandwich_prod.(Rt, circles) # rotate circles
 		]
 	end
-
 end
 
 function anim_rotor_fit()
